46a8880a8f3499992380ca201ce949f0b6f9b5e0
NikitaG <nrg1@tpu.ru>
2022-06-17 07:24:46 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using SFML;
+using SFML.Window;
+using SFML.Audio;
+using SFML.Graphics;
+using SFML.System;
+
+
+class Program
+{
+    static void Main()
+    {
+        RenderWindow window = new(new VideoMode(800, 600), "PTDSS");
+        CircleShape shape = new(100);
+
+        window.Closed += (obj, ev) => { window.Close(); };
+
+        shape.FillColor = Color.Green;
+        while (window.IsOpen)
+        {
+
+            window.DispatchEvents();
+            window.Draw(shape);
+            window.Display();
+
+        }
+    }
+}
+
